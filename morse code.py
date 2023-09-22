abf9dd3997aab73417b52417f08e5a9a31eb741e
Robin <126953395+Rob-squarerootofminus1-n@users.noreply.github.com>
2023-09-22 12:44:00 +0200

    Add files via upload

---
+++ morse code.py
@@ -0,0 +1,9 @@
+a = 14
+while a <16:
+    a=a+1
+    print ("j'ai", a, "ans")
+    
+    if a ==16:
+        print ("je peux commander le set lego téléscope hubble!")
+    else:
+        print ("oh non, je ne peux pas le commander :(")
